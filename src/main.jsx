26416326b0518e8fba25f967c33676b7e2bae779
Alina-end-art <martynovskaaalina93@gmail.com>
2026-04-09 15:36:10 +0300

    kill sw final

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+// 💥 УБИВАЕМ СТАРЫЙ SERVICE WORKER (PWA)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
